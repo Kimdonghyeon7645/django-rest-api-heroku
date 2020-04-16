58dcbc6d9822751caa0fbe4a556b46da5161cefc
Kimdonghyeon7645 <kkddhh2826@gmail.com>
2020-04-16 16:31:55 +0900

    fix : fix again

--- myapi/settings.py
+++ myapi/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = '!2l!u--itrfb!*zc1u19ou5_v$p&eagi)ydc@jpu4@07nz-c6y'
-SECRET_KEY = os.environ.get('DJANGO_KEY', '!2l!u--itrfb!*zc1u19ou5_v$p&eagi)ydc@jpu4@07nz-c6y')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '!2l!u--itrfb!*zc1u19ou5_v$p&eagi)ydc@jpu4@07nz-c6y')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
